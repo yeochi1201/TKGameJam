7f451932c8f0d0b72635aac5a5d7755768ceeea5
SsamE1 <127075967+SsamE1@users.noreply.github.com>
2023-11-04 11:43:27 +0900

    물약3종/물약먹기구현

---
+++ gamejam/Assets/Script/Shin/HpPotion.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HpPotion : Potion
+{
+    void Start()
+    {
+        potionType = PotionType.Hp;
+    }
+}
