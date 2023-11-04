da68c6239fa229fdfef046ad485b876d0026b59e
yeochi1201 <yeochi1201@gmail.com>
2023-11-04 15:13:40 +0900

    player ui

--- gamejam/Assets/Script/Network/PlayerUI.cs
+++ gamejam/Assets/Script/Network/PlayerUI.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Image skIcon;
+    public Image skcool;
+    public Slider HPbar;
+    public Image Minimap;
+
+    private PlayerController target;
+    public void setTarget(PlayerController target)
     {
-        
+        if(target == null)
+        {
+            Debug.LogError("Mssing Player for Player UI");
+        }
+        this.target = target;
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Update()
     {
-        
+        if(HPbar != null)
+        {
+            HPbar.value = target.Status.Hp;
+        }
+        if(target == null)
+        {
+            Destroy(this.gameObject);
+            return;
+        }      
     }
 }
