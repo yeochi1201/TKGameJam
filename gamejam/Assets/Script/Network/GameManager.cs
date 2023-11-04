bbe6d5c1c2a967ed8c35773d1a983d8ea7f1c154
yeochi1201 <yeochi1201@gmail.com>
2023-11-04 17:37:11 +0900

    Player Init + Serv

--- gamejam/Assets/Script/Network/GameManager.cs
+++ gamejam/Assets/Script/Network/GameManager.cs
@@ -55,16 +55,6 @@ public class GameManager : MonoBehaviourPunCallbacks
     public void GameStart()
     {
         PhotonNetwork.LoadLevel("BattleField");
-        if (playerPrefab == null)
-        {
-            Debug.LogError("<Color=Red><a>Missing</a></Color> playerPrefab Reference. Please set it up in GameObject 'Game Manager'", this);
-        }
-        else
-        {
-            Debug.LogFormat("We are Instantiating LocalPlayer from {0}", Application.loadedLevelName);
-            // we're in a room. spawn a character for the local player. it gets synced by using PhotonNetwork.Instantiate
-            PhotonNetwork.Instantiate(this.playerPrefab.name, new Vector3(0f, 5f, 0f), Quaternion.identity, 0);
-        }
     }
     #endregion
 
--- gamejam/Assets/Script/Network/PlayerLocalControl.cs
+++ gamejam/Assets/Script/Network/PlayerLocalControl.cs
@@ -1,12 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using ExitGames.Client.Photon.StructWrapping;
+using JetBrains.Annotations;
 
-public class LocalControl : MonoBehaviour
+public class PlayerLocalControl : MonoBehaviourPunCallbacks
 {
+    public GameObject playerPrefab;
+    
+    public List<Vector3> spawnpos;
+    private List<bool> spawned = new List<bool>();
     // Start is called before the first frame update
     void Start()
     {
+        if (playerPrefab == null)
+        {
+            Debug.LogError("<Color=Red><a>Missing</a></Color> playerPrefab Reference. Please set it up in GameObject 'Game Manager'", this);
+        }
+        else
+        {
+            Debug.LogFormat("We are Instantiating LocalPlayer from {0}", Application.loadedLevelName);
+            // we're in a room. spawn a character for the local player. it gets synced by using PhotonNetwork.Instantiate
+            PhotonNetwork.Instantiate(this.playerPrefab.name, choiceSpawnpos(spawnpos), Quaternion.identity, 0);
+        }
         
     }
 
@@ -14,5 +32,23 @@ public class LocalControl : MonoBehaviour
     void Update()
     {
         
+    }
+
+    [PunRPC]
+    private Vector3 choiceSpawnpos(List<Vector3> spawnpos)
+    {
+        PhotonView pv = playerPrefab.GetComponent<PhotonView>();
+
+        if (pv != null)
+        {
+            return spawnpos[pv.ViewID % 4];
+        }
+
+        else return Vector3.one;
+    }
+
+    void OnPhotonInstantiate(PhotonMessageInfo info)
+    {
+        info.Sender.TagObject = this.gameObject;
     }
 }
